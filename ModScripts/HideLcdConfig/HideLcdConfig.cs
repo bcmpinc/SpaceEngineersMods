1c7d651a3999e646966ec192c40d6020afddd0eb
Bauke Conijn <bcmpinc@users.sourceforge.net>
2022-08-20 12:50:09 +0200

    Small change to prevent crashes in HideLcdConfig.

--- ModScripts/HideLcdConfig/HideLcdConfig.cs
+++ ModScripts/HideLcdConfig/HideLcdConfig.cs
@@ -9,52 +9,57 @@ using VRage.ObjectBuilders;
 using VRage.Utils;
 
 namespace HideLcdConfig {
-    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
-    public class HideLcdConfig : MySessionComponentBase {
-        HashSet<string> Filtered = new HashSet<string>("Title,Rotate,PanelList,Content,Script,ScriptForegroundColor,ScriptBackgroundColor,ShowTextPanel,Font,FontSize,FontColor,alignment,TextPaddingSlider,BackgroundColor,ImageList,SelectTextures,ChangeIntervalSlider,SelectedImageList,RemoveSelectedTextures,PreserveAspectRatio".Split(','));
-        IMyTerminalControl[] AddedControls;
-        bool ShowState = false;
+  [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+  public class HideLcdConfig : MySessionComponentBase {
+    HashSet<string> Filtered = new HashSet<string>("Title,Rotate,PanelList,Content,Script,ScriptForegroundColor,ScriptBackgroundColor,ShowTextPanel,Font,FontSize,FontColor,alignment,TextPaddingSlider,BackgroundColor,ImageList,SelectTextures,ChangeIntervalSlider,SelectedImageList,RemoveSelectedTextures,PreserveAspectRatio".Split(','));
+    IMyTerminalControl[] AddedControls;
+    bool ShowState = false;
 
-        static bool HasLcd(IMyTerminalBlock block) => (block as Sandbox.ModAPI.Ingame.IMyTextSurfaceProvider)?.SurfaceCount > 0;
+    static bool HasLcd(IMyTerminalBlock block) => (block as Sandbox.ModAPI.Ingame.IMyTextSurfaceProvider)?.SurfaceCount > 0;
 
-        public override void LoadData() {
-            var Separate = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlSeparator, IMyTerminalBlock>("");
-            var Control = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlOnOffSwitch, IMyTerminalBlock>("ShowLcdConfig");
-            Control.Visible = x => true;
-            Control.Getter = x => ShowState;
-            Control.Setter = (x, v) => {
-                ShowState = v;
-                // HACK: Would be neater if I could call `RaiseVisibilityChanged()` directly, but that's not exposed in the Mod API.
-                x.ShowInToolbarConfig ^= true;
-                x.ShowInToolbarConfig ^= true;
-            };
-            Control.Title = MyStringId.GetOrCompute("Show LCD Controls");
-            Control.OnText = MyStringId.GetOrCompute("On");
-            Control.OffText = MyStringId.GetOrCompute("Off");
-            MyAPIGateway.TerminalControls.CustomControlGetter += TerminalControls_CustomControlGetter;
-            AddedControls = new IMyTerminalControl[] { Separate, Control };
-        }
-
-        public void TerminalControls_CustomControlGetter(IMyTerminalBlock block, List<IMyTerminalControl> controls) {
-            if (HasLcd(block)) {
-                var index = controls.FindIndex(x => x.Id == "PanelList");
-                controls.InsertRange(index, AddedControls);
-                //block.CustomData = string.Concat(controls.ConvertAll(x => x.Id+"\n"));
-                if (!ShowState) {
-                    string last = null;
-                    for (int i = 0; i < controls.Count; i++) {
-                        var id = controls[i].Id;
-                        if (Filtered.Contains(id)) {
-                            controls[i] = null;
-                        } else if (id == last) {
-                            controls[i] = null;
-                        } else {
-                            last = id;
-                        }
-                    }
-                    controls.RemoveAll(x => x == null);
-                }
-            }
-        }
+    public override void LoadData() {
+      var Separate = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlSeparator, IMyTerminalBlock>("");
+      var Control = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlOnOffSwitch, IMyTerminalBlock>("ShowLcdConfig");
+      Control.Visible = x => true;
+      Control.Getter = x => ShowState;
+      Control.Setter = (x, v) => {
+        ShowState = v;
+        // HACK: Would be neater if I could call `RaiseVisibilityChanged()` directly, but that's not exposed in the Mod API.
+        x.ShowInToolbarConfig ^= true;
+        x.ShowInToolbarConfig ^= true;
+      };
+      Control.Title = MyStringId.GetOrCompute("Show LCD Controls");
+      Control.OnText = MyStringId.GetOrCompute("On");
+      Control.OffText = MyStringId.GetOrCompute("Off");
+      MyAPIGateway.TerminalControls.CustomControlGetter += TerminalControls_CustomControlGetter;
+      AddedControls = new IMyTerminalControl[] { Separate, Control };
     }
+
+    public void TerminalControls_CustomControlGetter(IMyTerminalBlock block, List<IMyTerminalControl> controls) {
+      if (HasLcd(block)) {
+        var index = controls.FindIndex(x => x.Id == "PanelList");
+        if (index >= 0) {
+          controls.InsertRange(index, AddedControls);
+        } else { 
+          // Assume PanelList is missing because this mod is loaded twice and the changes already applied.
+          return; 
+        }
+
+        if (!ShowState) {
+          string last = null;
+          for (int i = 0; i < controls.Count; i++) {
+            var id = controls[i].Id;
+            if (Filtered.Contains(id)) {
+              controls[i] = null;
+            } else if (id == last) {
+              controls[i] = null;
+            } else {
+              last = id;
+            }
+          }
+          controls.RemoveAll(x => x == null);
+        }
+      }
+    }
+  }
 }
