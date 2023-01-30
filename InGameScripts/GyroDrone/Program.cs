31b26ec97ea1bf51f2444a852104a69936bac9c5
Bauke Conijn <bcmpinc@users.sourceforge.net>
2023-01-30 04:40:20 +0100

    Initial code for single thruster drone controller

--- InGameScripts/GyroDrone/Program.cs
+++ InGameScripts/GyroDrone/Program.cs
@@ -19,60 +19,55 @@ using VRage.Game.ObjectBuilders.Definitions;
 using VRageMath;
 
 namespace IngameScript {
-    partial class Program : MyGridProgram {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
+  partial class Program : MyGridProgram {
 
-        public Program() {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
-        }
+    IMyShipController       controller;
+    List<IMyGyro>           gyros      = new List<IMyGyro>();
+    List<IMyThrust>         thrusters  = new List<IMyThrust>();
 
-        public void Save() {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
-        }
-
-        public void Main(string argument, UpdateType updateSource) {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
-        }
+    public bool same_subgrid(IMyTerminalBlock block) {
+      return Me.CubeGrid == block.CubeGrid;
     }
+
+    public void assert(bool check, string error) {
+      if (check) return;
+      Echo("ERROR: " + error);
+      Runtime.UpdateFrequency = 0;
+      throw new Exception();
+    }
+
+    public Program() {
+      List<IMyShipController> controllers = new List<IMyShipController>();
+      
+      GridTerminalSystem.GetBlocksOfType(gyros,       same_subgrid); assert(gyros.Count > 0, "Missing gyros");
+      GridTerminalSystem.GetBlocksOfType(thrusters,   same_subgrid); assert(gyros.Count > 0, "Missing thrusters");
+      GridTerminalSystem.GetBlocksOfType(controllers, same_subgrid); assert(gyros.Count > 0, "Missing ship controllers");
+
+      controller = controllers[0];
+
+      Runtime.UpdateFrequency = UpdateFrequency.Update1;
+    }
+
+    public void Save() {
+      // Called when the program needs to save its state. Use
+      // this method to save your state to the Storage field
+      // or some other means. 
+      // 
+      // This method is optional and can be removed if not
+      // needed.
+    }
+
+    public void Main(string argument, UpdateType updateSource) {
+      var position = controller.GetPosition();
+      var gravity = controller.GetNaturalGravity();
+      
+      var thrust = thrusters.Sum(x=>x.MaxEffectiveThrust);
+      var mass = controller.CalculateShipMass().PhysicalMass;
+      var max_lift = thrust / mass;
+
+      var fraction = (float)(gravity.Length() / max_lift);
+
+      thrusters.ForEach(x => x.ThrustOverridePercentage = fraction);
+    }
+  }
 }
