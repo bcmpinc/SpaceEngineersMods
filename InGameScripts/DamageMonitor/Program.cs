b95b80675a545881d073fe61f935baec7829385c
Bauke Conijn <bcmpinc@users.sourceforge.net>
2023-02-01 10:39:40 +0100

    Not going to work as a PB.

--- InGameScripts/DamageMonitor/Program.cs
+++ InGameScripts/DamageMonitor/Program.cs
@@ -31,15 +31,57 @@ namespace IngameScript {
     public Program() {
       Runtime.UpdateFrequency = UpdateFrequency.Update100;
       GridTerminalSystem.GetBlocksOfType(screens, is_tagged);
-      screens.ForEach(x => {
-        x.ContentType = ContentType.TEXT_AND_IMAGE;
-        var r = new List<string>();
-        x.GetSprites(r);
-        x.CustomData = String.Join("\n",r);
-      });
+    }
+
+    void analyse_grid(Vector2 size, IMyCubeGrid grid, MySpriteDrawFrame frame) {
+      var center = size/2;
+      var scale = 10f;
+      for (var x=grid.Min.X; x<=grid.Max.X; x++) {
+        for (var y=grid.Min.Y; y<=grid.Max.Y; y++) {
+          bool block  = false;
+          bool damage = false;
+          bool fat    = false;
+          bool broken = false;
+          bool deform = false;
+          for (var z=grid.Min.Z; z<=grid.Max.Z; z++) {
+            if (grid.CubeExists(new Vector3I(x,y,z))) block = true;
+            var c = grid.GetCubeBlock(new Vector3I(x,y,z));
+            if (c == null) continue;
+            if (c.FatBlock != null) {
+              fat = true;
+              if (!c.FatBlock.IsFunctional) {
+                broken = true;
+              }
+            }
+            if (!c.IsFullIntegrity) damage = true;
+            if (c.HasDeformation) deform = true;
+          }
+
+          if (block) { 
+            Color c = Color.White;
+            /**/ if (broken)        c = Color.Red;
+            else if (damage && fat) c = Color.Orange;
+            else if (damage)        c = Color.Yellow;
+            else if (deform)        c = Color.Gray;
+            else if (fat)           c = Color.Blue;
+
+            var s = MySprite.CreateSprite("SquareSimple", center + new Vector2(x,-y) * scale, new Vector2(8,8));
+            s.Color = c;
+            frame.Add(s);
+          }
+        }
+      }
     }
 
     public void Main(string argument, UpdateType updateSource) {
+      screens.ForEach(x => {
+        x.ContentType = ContentType.SCRIPT;
+        x.Script = "";
+        var size = x.SurfaceSize;
+        var frame = x.DrawFrame();
+        analyse_grid(size, x.CubeGrid, frame);
+        frame.Dispose();
+      });
     }
   }
 }
